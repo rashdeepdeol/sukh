0663c07a6d0ae881bf49c45e2550b71fc0f3f6a3
Sukhdeep1124 <77212794+Sukhdeep1124@users.noreply.github.com>
2021-01-17 16:00:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,187 @@
 
-<?php
-$_Xmas = $_POST['xmas'];
-echo "$_Xmas";
+const quizData =[
 
-?>
+{
 
+	question : "What is the only function all C++ programs must contain?",
 
+	a: "Start()",
+	b: "System()",
+	c: "Program()",
+	d: "Main()",
+    correct:"d",
 
-const daysE1 = document.getElementById("days");
-const hoursE1 = document.getElementById('hours');
-const minutesE1 = document.getElementById('minutes');
-const secondsE1 = document.getElementById('seconds');
+},
+{
 
+	question : "Which of the following is the boolean operator for logical-and?",
 
+	a: "|",
+	b: "&",
+	c: "|&",
+	d: "&&",
+	correct:"d",
+},
+{
 
+	question : "Evaluate !(1 && !(0 || 1)).",
 
-var currentDate = $( ".post" ).datepicker( "getDate" );
+	a: "True",
+	b: "False",
+	c: "Unevaluate",
+	d: "None",
+    correct:"a",
+},
+{
 
+	question : "What punctuation is used to signal the beginning and end of code blocks?",
 
-function count(){
-	const xMasDate = new Date(xMas);
-	const currentDate = new Date();
-	const totalseconds = (xMasDate-currentDate)/1000;
+	a: "( and )",
+	b: "{ }",
+	c: "-> and",
+	d: "BEGIN and END)",
+    correct:"b",
+},
+{
 
+	question : "Which of the following is the correct operator to compare two variables?",
 
-    const days = Math.floor(totalseconds / 3600 / 24);
+	a: "|=",
+	b: "Equal",
+	c: "==",
+	d: "=",
+    correct:"c",
+},
+{
 
-    const hours = Math.floor((totalseconds / 3600)%24);
+	question : "What punctuation ends most lines of C++ code?",
 
-    const minutes = Math.floor ((totalseconds/60)%60);
-    
-    const seconds = Math.floor(totalseconds%60);  
-  
+	a: "; (semi-colon)",
+	b: "' (single quote)",
+	c: ". (dot)",
+	d: ": (colon)",
+    correct:"a",
+},
+{
 
-  daysE1.innerHTML =days;
-  hoursE1.innerHTML = hours;
-  minutesE1.innerHTML = minutes;
-  secondsE1.innerHTML = seconds;
+	question : "What is the correct value to return to the operating system upon the successful completion of a program?",
+
+	a: "-1",
+	b: "0",
+	c: "1",
+	d: "Program do not retuen a value",
+    correct:"b",
+},
+{
+
+	question : "Which of the following is a correct comment?",
+
+	a: "{ Comment }",
+	b: "/* Comment */",
+	c: "*/ Comments */",
+	d: "** Comment **",
+	correct:"b",
+},
+{
+
+	question : "Which of the following is not a correct variable type?",
+
+	a: "Float",
+	b: "Real",
+	c: "Int",
+	d: "Double",
+    correct:"b",
+},
+{
+
+	question : "Which of the following is true?",
+
+	a: "1",
+	b: "-1",
+	c: "88",
+	d: "All of above",
+	correct:"d",
+},
+
+]
+
+const quiz = document.getElementById("quiz");
+const questionXs = document.getElementById("question");
+const answerXs = document.querySelectorAll(".answer");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submit = document.getElementById("submit");
+
+let currentQ = 0;
+let score = 0;
+
+function load () {
+	const currentQData = quizData[currentQ];
+     questionXs.innerText = currentQData.question;
+     a_text.innerText = currentQData.a;
+     b_text.innerText = currentQData.b;
+     c_text.innerText = currentQData.c;
+     d_text.innerText = currentQData.d;
 
 }
 
-count();
-setInterval(count,1000);
+load();
 
+
+function getSelected () {
+
+      let answer = undefined;
+
+      answerXs.forEach((answerX)=> {
+            if(answerX.checked)
+            {
+            	answer = answerX.id;
+            }
+      	
+      });
+
+      return answer;
+
+	}
+
+function deselectAnswers () {
+	answerXs.forEach(answerX=> {
+            answerEL.checked = false;
+      	
+      });
+}
+
+submit.addEventListener("click", ()=>{
+
+  const answer = getSelected();
+   
+   if(answer){
+         if(answer === quizData[currentQ].correct){
+
+         	score++;
+         }
+         
+         currentQ++;
+
+         if(currentQ < quizData.length){
+         	load();
+         }
+         else{
+         	quiz.innerHTML = `<h2>You answered correctly ${score}/${quizData.length}</h2>`;
+         }
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+});
